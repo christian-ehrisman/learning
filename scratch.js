a77ecd19ab140fae05ef8587f17f95b8310b9937
Christian Ehrisman <christian.ehrisman@gmail.com>
2021-10-30 16:41:44 -0400

    parsing complex objects

---
+++ scratch.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+// Setup
+const contacts = [
+    {
+        firstName: "Akira",
+        lastName: "Laine",
+        number: "0543236543",
+        likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+        firstName: "Harry",
+        lastName: "Potter",
+        number: "0994372684",
+        likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+        firstName: "Sherlock",
+        lastName: "Holmes",
+        number: "0487345643",
+        likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+        firstName: "Kristian",
+        lastName: "Vos",
+        number: "unknown",
+        likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+];
+
+function lookUpProfile(name, prop) {
+    // Only change code below this line
+    for (let x in contacts) {
+        if (contacts[x].firstName == name) {
+            if (contacts[x][prop]) {
+                return contacts[x][prop]
+            }
+            else {
+                return "No such property."
+            }
+        }
+
+    }
+    return "No such contact"
+    // Only change code above this line
+}
+
+let out = lookUpProfile("Akira", "Address");
+console.log(out)
